56bdee3e4bfab21dac1519bdbe889f6ea80cc97c
Laure C <compointlaure@gmail.com>
2020-05-26 14:51:56 +0200

    delete methode

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -36,23 +36,7 @@ class ProductController extends Controller
      */
     public function store(Request $request)
     {
-        $validator = Validator::make($request->all(), [
-            'name' => 'required',
-            'description' => 'required',
-            'price' => 'required'
-        ]);
-         
-       
         
-                $product = new Product;
-                $product->id = $request->id;
-                $product->name = $request->name;
-                $product->description = $request->description;
-                $product->price = $request->price;
-              
-                $product->save();
-              
-                return $product;
             
     }
 
